0feb4017ba79e93a60f5bcc05259ccea7efc81c3
Woonsan Ko <w.ko@onehippo.com>
2016-11-11 16:16:36 -0500

    iniital creation

---
+++ site/src/main/java/com/github/woonsanko/examples/hippoboot/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.github.woonsanko.examples.hippoboot.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hippoonspringboot:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
