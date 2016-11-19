b0303a510348d1fc6a95d9d1299a2615316b5602
Woonsan Ko <w.ko@onehippo.com>
2016-11-19 23:08:28 +0100

    Adding remote debugging option

--- spring-boot-deploy/test.sh
+++ spring-boot-deploy/test.sh
@@ -8,6 +8,7 @@ cp -R ../cms/target/cms $EMBEDDED_CATALINA_BASE/webapps/
 cp -R ../site/target/site $EMBEDDED_CATALINA_BASE/webapps/
 
 java -Xms512m -Xmx1024m \
+    -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n \
     -Drepo.path="target/storage" \
     -jar target/hippo-on-spring-boot-spring-boot-deploy-0.1.0-SNAPSHOT.jar \
     --embedded.catalina.base=$EMBEDDED_CATALINA_BASE
