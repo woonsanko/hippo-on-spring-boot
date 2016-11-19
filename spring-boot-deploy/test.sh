973e54ec3e36c17bd8fcf303b12895917b6f1e29
Woonsan Ko <w.ko@onehippo.com>
2016-11-19 22:47:22 +0100

    adding jvm heap size option

--- spring-boot-deploy/test.sh
+++ spring-boot-deploy/test.sh
@@ -7,6 +7,7 @@ mkdir $EMBEDDED_CATALINA_BASE/webapps
 cp -R ../cms/target/cms $EMBEDDED_CATALINA_BASE/webapps/
 cp -R ../site/target/site $EMBEDDED_CATALINA_BASE/webapps/
 
-java -Drepo.path="target/storage" \
+java -Xms512m -Xmx1024m \
+    -Drepo.path="target/storage" \
     -jar target/hippo-on-spring-boot-spring-boot-deploy-0.1.0-SNAPSHOT.jar \
     --embedded.catalina.base=$EMBEDDED_CATALINA_BASE
