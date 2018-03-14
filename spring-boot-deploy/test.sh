d7649ffe62463361e3f8c058c1080dcf6af19860
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-03-14 11:38:45 -0400

    Upgrading demo to v12.2

--- spring-boot-deploy/test.sh
+++ spring-boot-deploy/test.sh
@@ -19,4 +19,4 @@ java \
     -Xms512m -Xmx1024m \
     -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n \
     -Dproject.basedir="../" \
-    -jar target/hippo-on-spring-boot-spring-boot-deploy-0.1.0-SNAPSHOT.jar "$@"
+    -jar target/hippo-on-spring-boot-spring-boot-deploy-1.0.0-SNAPSHOT.jar "$@"
